ec29a7856407b2ae9ce0a2e17d7abab97578e137
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-07-22 17:18:46 -0300

    refactor: rework TestePlayer with vector-based movement, physics and variable jump

--- src/components/entities/teste_player.py
+++ src/components/entities/teste_player.py
@@ -1,5 +1,6 @@
 import pygame
 from src.components.collider import Collider
+from pygame.math import Vector2
 
 
 class TestePlayer():
@@ -8,19 +9,30 @@ class TestePlayer():
         self._y = y
         self.scene = scene
         self.sprite = scene.add_image('mario_idle', x, y, 'midbottom')
-
-        self.vel_x = 5
-        self.vel_y = 0
-        self.gravity = False
-        self.direction_facing = 'left'
-        self.sprite.set_scale(3)
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(0, 0)
+        self.direction = Vector2(0, 0)
+        self.move_speed = 120  # px/s
+        self.is_on_ground = False
+        print(self.sprite.height)
         self.hitbox = Collider(x, y, self.sprite.width,
                                self.sprite.height, anchor='midbottom')
-        self.original_image = self.sprite.texture
+        self.jump_force = -210
+
         self.sprite.chroma_key(0, 116, 116)
         self.state = 'idle'
         self.move_disable = False
-        self.is_on_ground = False
+
+        self.jump_time = 0
+        self.min_jump_time = 0.1
+        self.max_jump_time = 0.25  # em segundos
+        self.is_jumping = False
+
+        self.direction_facing = 'left'
+        self.face_side = {
+            1: self._face_right,
+            -1: self._face_left,
+        }
 
     @property
     def x(self):
@@ -42,133 +54,109 @@ class TestePlayer():
         self.sprite.y = value
         self.hitbox.y = value
 
-    def char_command(self):
+    def handle_input(self):
         keys = pygame.key.get_pressed()
+        left = keys[pygame.K_LEFT] or keys[pygame.K_a]
+        right = keys[pygame.K_RIGHT] or keys[pygame.K_d]
 
-        try_look_up = keys[pygame.K_w]
-        try_duck = keys[pygame.K_s]
-        try_jump = keys[pygame.K_SPACE]
-        try_walk = keys[pygame.K_a] or keys[pygame.K_d]
+        # Cancelamento mútuo
+        if left and right:
+            self.direction.x = 0
+        elif left:
+            self.direction.x = -1
+        elif right:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
 
-        # 🧍‍♂️ Sai do estado de olhar pra cima se soltou W
-        if self.state == 'looking_up' and not try_look_up:
-            self.move_disable = False
-            if try_walk:
-                self.state = 'walking'
+        if keys[pygame.K_SPACE] and self.is_on_ground:
+            self.is_jumping = True
+            self.jump_time = 0
+            self.velocity.y = self.jump_force
+
+    def apply_physics(self):
+        dt = self.scene.game.delta_time
+        acceleration = 600  # px/s²
+        deceleration = 800  # px/s²
+        max_speed = self.move_speed
+
+        # Pulo variável: enquanto segurando e dentro do tempo máximo
+        if self.is_jumping:
+            self.jump_time += dt
+            if self.jump_time >= self.max_jump_time:
+                self.is_jumping = False
+            elif not pygame.key.get_pressed()[pygame.K_SPACE] and self.jump_time >= self.min_jump_time:
+                self.is_jumping = False
             else:
-                self.state = 'idle'
+                # Aplica gravidade reduzida (opcional)
+                self.velocity.y += self.scene.gravity * 0.2 * dt
+        else:
+            if not self.is_on_ground:
+                self.velocity.y += self.scene.gravity * dt
 
-        # 🧎 Sai do estado de agachado se soltou S
-        elif self.state == 'ducked' and not try_duck:
-            self.move_disable = False
-            if try_walk:
-                self.state = 'walking'
-            else:
-                self.state = 'idle'
+         # Movimento horizontal com aceleração e desaceleração
+        if self.direction.x != 0:
+            self.velocity.x += self.direction.x * acceleration * dt
+            if abs(self.velocity.x) > max_speed:
+                self.velocity.x = max_speed * self.direction.x
+        else:
+            if self.velocity.x > 0:
+                self.velocity.x -= deceleration * dt
+                if self.velocity.x < 0:
+                    self.velocity.x = 0
+            elif self.velocity.x < 0:
+                self.velocity.x += deceleration * dt
+                if self.velocity.x > 0:
+                    self.velocity.x = 0
 
-        # 🧎‍♂️ Agachar tem prioridade
-        elif try_duck:
-            self.state = 'ducked'
-            self.move_disable = True
-
-        # 👆 Olhar cima vem depois
-        elif try_look_up:
-            self.state = 'looking_up'
-            self.move_disable = True
-
-        # 🚶 Andar, só se não estiver travado
-        elif try_walk and not self.move_disable:
-            self.state = 'walking'
-
-        elif try_jump and not self.move_disable and not self.state == 'air':
-            self.state = 'jumping'
-
-        # 💤 Ninguém tá fazendo nada
-        # else:
-        #     self.state = 'idle'
-        #     self.move_disable = False
-
-    def char_states(self):
-
-        keys = pygame.key.get_pressed()
-        if self.state == 'air':
-            return
-
-        if keys[pygame.K_a]:
-            self.direction_facing = 'left'
-        if keys[pygame.K_d]:
+    def _face_right(self):
+        if self.direction_facing != 'right':
             self.direction_facing = 'right'
-
-        if self.state == 'idle':
-            self.sprite.set_texture('mario_idle')
-
-        elif self.state == 'looking_up':
-            self.sprite.set_texture('mario_look_up')
-
-        elif self.state == 'walking':
-            self.sprite.set_texture('mario_walking')
-            if keys[pygame.K_a]:
-                self.x -= self.vel_x
-            if keys[pygame.K_d]:
-                self.x += self.vel_x
-
-        elif self.state == 'ducked':
-            self.sprite.set_texture('mario_duck')
-
-        elif self.state == 'jumping':
-            self.state = 'air'
-            if keys[pygame.K_SPACE] and self.is_on_ground:
-
-                self.sprite.set_texture('mario_idle')
-
-                self.vel_y = - 733
-
-        if keys[pygame.K_w]:
-            self.y -= 10
-        if keys[pygame.K_s]:
-            self.y += 10
-
-        if self.direction_facing == 'right':
-            print('flipou pra direita')
             self.sprite.texture = pygame.transform.flip(
                 self.sprite.texture, True, False)
-        elif self.direction_facing == 'left':
-            print('flipou pra esquerda')
-            self.sprite.texture = pygame.transform.flip(
-                self.sprite.texture, False, False)
+
+    def _face_left(self):
+        if self.direction_facing != 'left':
+            self.direction_facing = 'left'
+            self.sprite.texture = self.sprite.texture = pygame.transform.flip(
+                self.sprite.texture, True, False)
+
+    def move(self):
+        self.face_side.get(int(self.direction.x), lambda: None)()
+        # print((int(self.direction.x)))
+        dt = self.scene.game.delta_time
+        # Aplicar movimento
+        self.x += self.velocity.x * dt
+        # self.resolve_collisions(tiles, axis='x')
+
+        self.y += self.velocity.y * dt
+        # self.resolve_collisions(tiles, axis='y')
 
     def update(self):
-        print(self.state)
-        if not self.is_on_ground:
-            self.vel_y += self.scene.gravity * self.scene.game.delta_time
+        self.handle_input()
+        self.apply_physics()
+        self.move()
 
-        self.y += self.vel_y * self.scene.game.delta_time
-
-        self.char_command()
-        self.char_states()
         self.terrain_collisions()
 
     def terrain_collisions(self):
         self.is_on_ground = False
         for terrain in self.scene.terrains:
-            if terrain.get_collision_side(self.hitbox):
-                if terrain.is_solid:
-                    side = terrain.get_collision_side(self.hitbox)
-                    terrain.block_overlap(self.hitbox)
-                    self._sync_position_from_hitbox()
-                    if side == 'top':
-                        self.is_on_ground = True
-                        # print('ta no chao')
+            side = terrain.get_collision_side(self.hitbox)
+            if side == 'top' and terrain.is_solid and self.velocity.y >= 0:
+                terrain.block_overlap(self.hitbox)
+                self._sync_position_from_hitbox()
+                self.is_on_ground = True
 
-                    # print(side)
+            if side == 'bottom' and terrain.is_solid and self.velocity.y <= 0:
+                terrain.block_overlap(self.hitbox)
+                self._sync_position_from_hitbox()
+                self.is_jumping = False
+
+                # print('ta no chao')
+
+                # print(side)
 
     def _sync_position_from_hitbox(self):
         self.x = self.hitbox.x
         self.y = self.hitbox.y
-
-    # def is_on_ground(self):
-    #     for terrain in self.scene.terrains:
-    #         print(terrain.get_collision_side(self.hitbox))
-    #         if terrain.get_collision_side(self.hitbox) == 'top':
-    #             return True
-    #     return False
